acebfa55bded768182377c18b6e01b493261f202
sarah <sarahabimay@gmail.com>
2015-02-04 15:56:36 +0000

    replace ajax jquery with js

--- guardian.js
+++ guardian.js
@@ -1,99 +1,71 @@
+function reloadNewsBySection( section ) {
+	var apiURL = "http://content.guardianapis.com/search?section=" + section + "&api-key=test";
+	getResultsJSON( apiURL );	
+};
+
+function getResultsJSON( apiRequestURL ) {
+    var request;
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        request = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    request.open("GET", apiRequestURL, true);
+    request.send();
+
+    // callback function that will get called once request ready state changes
+    //  or: request.onload = function() {}
+    request.onreadystatechange = function() {
+		//if (request.status >= 200 && request.status < 400) {
+		// we want to process the response once the readyState = 4 e.g.
+        if ( request.readyState == 4 ) {
+           if( request.status == 200){				          		
+          		var data = JSON.parse( request.responseText);
+          		data.response.results.map( function( element, index ) {
+						var html = "<li><a class='+ element.sectionId + '-article' href='"+ element.webUrl +  "'>" +element.webTitle + '</a></li>';
+						$('#'+element.sectionId + '-result').append( html );
+			        });               
+           }
+           else if(  request.status == 400) {
+      
+           }
+           else {
+               alert('something else other than 200 was returned')
+           }
+        }
+    };
+
+    // another callback which will be triggered if there is an error
+    request.onerror = function() {
+  		// There was a connection error of some sort
+  		console.log( 'ERROR: ' );
+	};
+};
+
 
 $(document).ready(function(){
 	
 	$('.a-uknews').click(function(e){		
 		$('#uknews-result').empty();
-		$.getJSON("http://content.guardianapis.com/search?section=uk-news&api-key=test", function(data, status){
-	    $.each( data.response.results, function( index, element ) {
-				var html = "<li><a class='uknews-article' href='"+ element.webUrl +  "'>" +element.webTitle + '</a></li>';
-				$('#uknews-result').append( html );
-	        });
-	    });
+		reloadNewsBySection( 'uk-news' );	
 	}),
 
 	$('.a-football').click(function(e){		
 		$('#football-result').empty();
-		$.getJSON("http://content.guardianapis.com/search?section=football&api-key=test", function(data, status){
-	    $.each( data.response.results, function( index, element ) {
-				var html = "<li><a class='football-article' href='"+ element.webUrl +  "'>" +element.webTitle + '</a></li>';
-				$('#football-result').append( html );
-	        });
-	    });
+		reloadNewsBySection( 'football' );
 	}),
 
 	$('.a-travel').click(function(e){		
 		$('#travel-result').empty();
-		$.getJSON("http://content.guardianapis.com/search?section=travel&api-key=test", function(data, status){
-	    $.each( data.response.results, function( index, element ) {
-				var html = "<li><a class='travel-article' href='"+ element.webUrl +  "'>" +element.webTitle + '</a></li>';
-				$('#travel-result').append( html );
-	        });
-	    });
+		reloadNewsBySection( 'travel')
 	}),
 	
 	// This call to the Guaradian API gets called on initial loading of the webpage in order to populate the active page
-	// straight away.  Only need to populate the active page to save on unneccessary calls to the API
-	$.getJSON("http://content.guardianapis.com/search?section=uk-news&api-key=test", function(data, status){
-	    $.each( data.response.results, function( index, element ) {
-				var html = "<li><a class='uknews-article' href='"+ element.webUrl +  "'>" +element.webTitle + '</a></li>';
-				$('#uknews-result').append( html );
-	        });
-    });
+	// straisght away.  Only need to populate the active page to save on unneccessary calls to the API
+	reloadNewsBySection( 'uk-news');
 
-	/*$.getJSON("http://content.guardianapis.com/search?section=football&api-key=test", function(data, status){
-        $.each( data.response.results, function( index, element ) {
-			var html = "<li><a class='football-article' href='"+ element.webUrl +  "'>" +element.webTitle + '</a></li>';
-			$('#football-result').append( html );
-        });
-    });
 	
-	$.getJSON("http://content.guardianapis.com/search?section=travel&api-key=test", function(data, status){
-        $.each( data.response.results, function( index, element ) {
-			var html = "<li><a class='travel-article' href='"+ element.webUrl +  "'>" +element.webTitle + '</a></li>';
-			$('#travel-result').append( html );
-        });
-    });
-   */
 });
-/*
-$(document).ready(function(){
-	
-	$('#sections #uknews').click(function(e){		
-		var $root = $('#uknews-result')
-		$.getJSON("http://content.guardianapis.com/search?api-key=kxna6pxmdg7q35pjvhhkh2m9", function(data, status){
-	        $.each( data.response.results, function( index, element ) {
-				var html = "<li><a href='"+ element.webUrl +  "'>" +element.webTitle + '</a></li>';
-				$root.append( html );
-	        })
-        });
-	}),
-	$('#sections #football').click(function(e){
-		$('.nav-tabs li').removeClass('active'); // remove active class from tabs
-    	$(this).addClass('active');
-    	//$('.tab-content div').removeClass('active');
-		//$('.tab-pane #football').addClass( 'active');
-	    $('.tab-content div').hide(); // hide all tab content
-    	$('.tab-content #football').show(); // show the tab content with matching id
-		var $root = $('#football-result')
-		$.getJSON("http://content.guardianapis.com/search?api-key=kxna6pxmdg7q35pjvhhkh2m9&ion=football", function(data, status){
-		//$.getJSON("http://content.guardianapis.com/search?api-key=kxna6pxmdg7q35pjvhhkh2m9&ion=football", function(data, status){
-            $.each( data.response.results, function( index, element ) {
-				var html = "<li><a href='"+ element.webUrl +  "'>" +element.webTitle + '</a></li>';
-				$root.append( html );
-            })
-        });
-	});
-	
-	$('#sections #travel').click(function(e){
-		var $root = $('#travel-result')
-		$.getJSON("http://content.guardianapis.com/search?api-key=kxna6pxmdg7q35pjvhhkh2m9&ion=travel", function(data, status){
-		//$.getJSON("http://content.guardianapis.com/search?api-key=kxna6pxmdg7q35pjvhhkh2m9&ion=football", function(data, status){
-            $.each( data.response.results, function( index, element ) {
-				var html = "<li><a href='"+ element.webUrl +  "'>" +element.webTitle + '</a></li>';
-				$root.append( html );
-            })
-        });
-    });
-
-})
-*/
